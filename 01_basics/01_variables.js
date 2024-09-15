34c8060e5a054fe71111bcdf7471e424462cdc62
Rajesh-Ransingh <144913160+Rajesh-Ransingh@users.noreply.github.com>
2024-09-15 11:32:18 +0000

    container config done

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345;
+let accountEmail = "rajesh@gmail.com";
+var accountPassword = "rajesh@123";
+accountCity = "Banpur";
+
+console.log(accountId);
+console.log(accountEmail);
+console.log(accountPassword);
+console.log(accountCity);
+console.table([accountId , accountEmail , accountPassword , accountCity]);
