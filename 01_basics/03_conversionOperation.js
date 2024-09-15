e86b6054128cb450df6bc2fbe572042353f466a2
Rajesh-Ransingh <144913160+Rajesh-Ransingh@users.noreply.github.com>
2024-09-15 16:55:44 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+let score = 33;
+console.log(typeof score);
+console.log(typeof(score));
+let score1 = "33";
+console.log(typeof score1);
+let valueIntNumber = Number(score1);
+console.log(typeof valueIntNumber);
+let score2 = "33abc";
+let valueIntNumber1 = Number(score2);
+console.log(valueIntNumber1);
+console.log(typeof valueIntNumber1);
+
+let isLoggedIn = 1;
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 = true; 0 = false
+// "" = false ; Rajesh =  true;
